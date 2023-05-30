444e92bdc78fd4305455292ae41344cc727612f7
ash <133685384+bubble-waffle@users.noreply.github.com>
2023-05-30 15:08:41 -0300

    30/5 commit
    basic css, outlined js

---
+++ script.js
@@ -0,0 +1,29 @@
+
+//list of all items, levels
+
+///functions///
+
+function starGacha(){ //determine star level
+    pass;
+}
+//add pulls into inventory
+
+//add pulls into history
+
+//pity resetter
+
+//50/50 status
+
+//edit banner
+
+//print gacha results
+
+//pulling action
+
+//open history
+
+//open inventory
+
+//the HISTORY
+
+//inventory display(consts)
